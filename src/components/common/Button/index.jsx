21f37b4e1f76e0c3ee3643c3c44a0274b38450f2
RaminduA <raminduanjana22171@gmail.com>
2022-07-11 21:15:32 +0530

    Added a new page

---
+++ src/components/common/Button/index.jsx
@@ -0,0 +1,77 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+import { Button } from "@mui/material";
+
+class RButton extends Component{
+    static propTypes = {
+        onclick: PropTypes.func,
+        children: PropTypes.node,
+        variant: PropTypes.string,
+        className: PropTypes.any,
+        label: PropTypes.string,
+        size: PropTypes.string,
+        disables: PropTypes.bool,
+        color: PropTypes.string,
+        type: PropTypes.string,
+        startIcon: PropTypes.string,
+        endIcon: PropTypes.string
+    }
+
+    static defaultProps = {
+        className: "",
+        color: "primary",
+        label: "",
+        size: 'medium',
+        variant: "contained",
+        disabled: false,
+        type: "button"
+    }
+
+    handleButtonClick = (event) => {
+        const {onClick, disabled} = this.props;
+
+        if(disabled) return;
+         
+        onClick && onClick({event});
+    }
+
+    renderChildren = (label, children) => {
+        if(label) return label;
+        if(children)  return children;
+    }
+
+    render() {
+        const {
+            children,
+            label,
+            className,
+            color,
+            size,
+            type,
+            variant,
+            disabled,
+            startIcon,
+            endIcon,
+            style,
+        } = this.props;
+
+        return(
+            <Button
+                className={className}
+                size={size}
+                endIcon={endIcon}
+                startIcon={startIcon}
+                variant={variant}
+                disabled={disabled}
+                color={color}
+                type={type}
+                onClick={this.handleButtonClick}
+                style={style}
+            >
+                {this.renderChildren(label, children)}
+            </Button>
+        )
+    }
+}
+
+export default RButton;
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,66 @@
+import { withStyles } from '@mui/styles';
+import { Component } from 'react';
+import RButton from '../../components/common/Button';
+import not_found from '../../assets/img/not_found.jpg';
+import { Link } from 'react-router-dom';
+
+const styleSheet = () => ({
+    container: {
+        position: 'relative',
+        display: 'flex',
+        margin: '0',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    img_container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0',
+        height: '100vh',
+        width: '100vw',
+        maxHeight: '100vh',
+        maxWidth: '100vw'
+    },
+    img: {
+        margin: '0',
+        height: '100%',
+        width: '100%'
+    },
+    Link: {
+        position: 'absolute',
+        top: '75%',
+        margin: '0',
+        color: 'green',
+        transform: 'translateX(-10%)'
+    }
+
+})
+
+
+class NotFound extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let { classes } = this.props;
+        return (
+            <div className={classes.container}>
+                <div className={classes.img_container}>
+                    <img className={classes.img} src={not_found} alt="" />
+                </div>
+                <Link className={classes.Link} to="/home">
+                    <RButton
+                        color="success"
+                        variant="contained"
+                        size="large"
+                        label="Back to home page"
+                    />
+                </Link>
+            </div >
+        )
+    }
+}
+export default withStyles(styleSheet)(NotFound);
